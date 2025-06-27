679be176a1ece6349a5d92d7390d73883657d464
virtualheet <heetvavadiya456@gmail.com>
2025-06-27 08:55:10 +0530

    complete gemini to summary of commits functions added

---
+++ src/lib/gemini.ts
@@ -0,0 +1,76 @@
+import { GoogleGenerativeAI } from "@google/generative-ai"
+import { Document } from "@langchain/core/documents";
+
+const genai = new GoogleGenerativeAI(process.env.GEMINI_API_KEY!);
+const model = genai.getGenerativeModel({
+    model: "gemini-2.5-flash",
+});
+
+export const aiSummarize = async (diff: string) => {
+    const prompt = `You are an expert programmer, and you are trying to summarize a git diff.
+    Reminders about the git diff format:
+    For every file, there are a few metadata lines, like (for example):
+    \`\`\`
+    diff --git a/lib/index.js b/lib/index.js
+    index aadf691..bfef603 100644
+    --- a/lib/index.js
+    +++ b/lib/index.js
+    \`\`\`
+    This means that \`lib/index.js\` was modified in this commit. Note that this is only an example.
+    Then there is a specifier of the lines that were modified.
+    A line starting with \`+\` means it was added.
+
+    A line that starting with \`-\` means that line was deleted.
+    A line that starts with neither \`+\` nor \`-\` is code given for context and better understanding.
+
+     EXAMPLE SUMMARY COMMENTS:
+       \`\`\`
+    * Raised the amount of returned recordings from \`10\` to \`100\` [packages/server/recordings_api.ts], [packages/server/constants.ts]
+    * Fixed a typo in the github action name [.github/workflows/gpt-commit-summarizer.yml]
+    * Moved the \`octokit\` initialization to a separate file [src/octokit.ts], [src/index.ts]
+    * Added an GEMINI API for completions [packages/utils/apis/gemini.ts]
+    * Lowered numeric tolerance for test files
+        \`\`\`
+    Most commits will have less comments than this examples list.
+    The last comment does not include the file names,
+    because there were more than two relevant files in the hypothetical commit.
+    Do not include parts of the example in your summary.
+    It is given only as an example of appropriate comments
+
+    Please summarise the following diff file: \n\n${diff}
+    `;
+
+    const response = await model.generateContent(prompt);
+    return response.response.text();
+}
+
+export const summarizeCode = async (doc: Document) => {
+    console.log("getting summary for :", doc.metadata.source);
+    try {
+        const code = doc.pageContent.slice(0, 10000);
+        const response = await model.generateContent([
+            `You are an intelligent senior software engineer who specialise in onboarding junior software engineers onto projects`,
+            `You are onboarding a junior software engineer and explaining to them the purpose of the ${doc.metadata.source} file
+            here is the code:
+            ---
+            ${code}
+            ---
+            give a summary no more than 100 words of the code above`
+        ]);
+        return response.response.text();
+    } catch (error) {
+        return 'error in summarize code';
+    }
+
+}
+
+
+export const generateEmbedding = async (summary: string) => {
+        const model = genai.getGenerativeModel({
+            model: "text-embedding-004",
+        });
+
+        const response = await model.embedContent(summary);
+        const embedding = response.embedding;
+        return embedding.values;
+    }
---
+++ src/lib/github.ts
@@ -0,0 +1,121 @@
+import { db } from "@/server/db";
+import { Octokit } from "octokit";
+import { aiSummarize } from "@/lib/gemini";
+import axios from "axios";
+
+const octokit = new Octokit({
+    auth: process.env.GITHUB_TOKEN,
+});
+
+
+type Response = {
+    commitHash: string;
+    commitMessage: string;
+    commitAuthorName: string;
+    commitAuthorAvatar: string;
+    commitDate: string;
+}
+
+export const getCommits = async (githubUrl: string, page = 1, per_page = 30): Promise<Response[]> => {
+    const urlParts = githubUrl.split("/");
+    const owner = urlParts[urlParts.length - 2];
+    const repo = urlParts[urlParts.length - 1];
+    
+    if (!owner || !repo) {
+        throw new Error("Invalid GitHub URL");
+    }
+    
+    const { data } = await octokit.rest.repos.listCommits({
+        owner,
+        repo,
+        page,
+        per_page,
+    });
+
+    const sortedCommits = data.sort((a, b) => {
+        const dateA = a.commit.author?.date || '';
+        const dateB = b.commit.author?.date || '';
+        return new Date(dateB).getTime() - new Date(dateA).getTime();
+    });
+
+
+    return sortedCommits.map((commit) => ({
+        commitHash: commit.sha,
+        commitMessage: commit.commit.message,
+        commitAuthorName: commit.commit.author?.name || '',
+        commitAuthorAvatar: commit.author?.avatar_url || '',
+        commitDate: commit.commit.author?.date || '',
+    }));
+};
+
+export const pollCommits = async (projectId: string, page = 1) => {
+    const { project, githubUrl } = await fetchProjectGithubUrl(projectId);
+    if (!githubUrl) {
+        throw new Error("GitHub URL not found");
+    }
+    
+    const commitHashes = await getCommits(githubUrl, page);
+    const unprocessedCommits = await filterUnProcessedCommits(projectId, commitHashes);
+
+    if (unprocessedCommits.length === 0) {
+        return; // Exit if no new commits to process
+    }
+
+    const summariesResponses = await Promise.allSettled(unprocessedCommits.map(async (commit) => {
+        return await summarizeCommits(githubUrl, commit.commitHash);
+    }));
+
+    const summaries = summariesResponses.map((response) => {
+        if (response.status === "fulfilled") {
+            return response.value as string;
+        }
+        return "No summary available";
+    });
+
+    return await db.commit.createMany({
+        data: summaries.map((summary, index) => ({
+            projectId,
+            commitHash: unprocessedCommits[index]!.commitHash,
+            commitMessage: unprocessedCommits[index]!.commitMessage,
+            commitAuthorName: unprocessedCommits[index]!.commitAuthorName,
+            commitAuthorAvatar: unprocessedCommits[index]!.commitAuthorAvatar,
+            commitDate: unprocessedCommits[index]!.commitDate,
+            summary,
+        })),
+    });
+};
+
+const summarizeCommits = async (githubUrl:string, commitHash: string) => {
+    const { data } = await axios.get(`${githubUrl}/commit/${commitHash}.diff`,{
+        headers: {
+            "Accept": "application/vnd.github.v3.fiff",
+        },
+    });
+    
+    return await aiSummarize(data) || "No summary available";
+}
+
+
+async function fetchProjectGithubUrl(projectId: string) {
+    const project = await db.project.findUnique({
+        where: {
+            id: projectId,
+        },
+        select: {
+            githubUrl: true,
+        },
+    });
+    return { project, githubUrl: project?.githubUrl };
+}
+
+async function filterUnProcessedCommits(projectId: string, commitHashes: Response[]) {
+    const processedCommits = await db.commit.findMany({
+        where: { projectId },
+    });
+    const unProcessedCommits = commitHashes.filter((commit) => !processedCommits.some((processedCommit) => processedCommit.commitHash === commit.commitHash));
+    return unProcessedCommits;
+}
+
+
+// await pollCommits("cm6wc7o0r000011h7hiv7dipj").then(console.log);
+// console.log("done");
